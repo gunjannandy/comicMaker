42ab329f5087f6266f92929cca8707db61e40a7f
Gunjan Nandy <gunjan.nandy1@gmail.com>
2019-06-16 22:05:03 +0530

    indentation fix

--- comicMaker/mangaLike.py
+++ comicMaker/mangaLike.py
@@ -30,7 +30,7 @@ def mangaLike():
 	originDirectory=os.getcwd()
 	os.chdir('..')
 	if not os.path.exists('comicDownloads'+os.sep):
-       os.makedirs('comicDownloads'+os.sep)
+		os.makedirs('comicDownloads'+os.sep)
 	os.chdir('comicDownloads'+os.sep)
 	for comicName in library:
 		incompleteUrl="https://mangalike.net/manga/"+comicName+"/"
